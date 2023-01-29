aff7b4c141e359697a917346329b1f0d30c0c0c2
i-sanjay-cs <sanjay826870@gmail.com>
2023-01-29 13:46:24 +0530

    new

---
+++ src/Team.jsx
@@ -0,0 +1,14 @@
+const Team = ({selectedTeam,handleTeamSelectionChange}) =>
+  {
+   return(
+
+<select className="form-select form-select-lg" value={selectedTeam} onChange= {handleTeamSelectionChange}>
+      <option>TeamA</option>
+      <option>TeamB</option>
+      <option>TeamC</option>
+      <option>TeamD</option>
+    </select>
+   ) 
+  }
+
+export default Team;
